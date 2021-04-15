adf9b335bdeda66c1be97bfc378c4a159ad3e92e
gmidorii <stitow14@gmail.com>
2021-04-15 23:33:18 +0900

    add no11.

---
+++ algorithms/11.container-with-most-water.go
@@ -0,0 +1,33 @@
+package main
+
+func maxArea(height []int) int {
+	rHeight := make([]int, len(height))
+	j := 0
+	for i := len(height) - 1; i >= 0; i-- {
+		rHeight[j] = height[i]
+		j++
+	}
+
+	max := 0
+	l := len(height)
+	for i, v := range height {
+		for j, rv := range rHeight {
+			if i+j >= l {
+				break
+			}
+			w := (l - j) - (i + 1)
+			h := 0
+			if rv > v {
+				h = v
+			} else {
+				h = rv
+			}
+			area := w * h
+			if area > max {
+				max = area
+			}
+		}
+	}
+
+	return max
+}
