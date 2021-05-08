4840b30a29c71e4b7aff0b8007cd3f45b010600a
gmidorii <stitow14@gmail.com>
2021-05-08 23:52:28 +0900

    update no24 test.

--- algorithms/24.swap-nodes-in-pairs_test.go
+++ algorithms/24.swap-nodes-in-pairs_test.go
@@ -45,10 +45,69 @@ func Test_swapPairs(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "",
+			args: args{
+				head: nil,
+			},
+			want: nil,
+		},
+		{
+			name: "",
+			args: args{
+				head: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val: 5,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 5,
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		if got := swapPairs(tt.args.head); !reflect.DeepEqual(got, tt.want) {
 			t.Errorf("%q. swapPairs() = %v, want %v", tt.name, got, tt.want)
+			var index int
+			for {
+				if got == nil && tt.want == nil {
+					break
+				} else if got == nil || tt.want == nil {
+					t.Errorf("%v got:%v, want:%v", index, got, tt.want)
+					break
+				}
+				t.Logf("got: %v", got.Val)
+				t.Logf("want: %v", tt.want.Val)
+				if got.Val != tt.want.Val {
+					t.Errorf("%v got:%v, want:%v", index, got.Val, tt.want.Val)
+				}
+				got = got.Next
+				tt.want = tt.want.Next
+				index++
+			}
 		}
 	}
 }
