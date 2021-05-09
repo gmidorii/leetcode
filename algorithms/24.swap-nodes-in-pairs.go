da002d2aa32a459407c04dcacccc870b952386d1
gmidorii <stitow14@gmail.com>
2021-05-09 22:22:07 +0900

    add no24 sample.

--- algorithms/24.swap-nodes-in-pairs.go
+++ algorithms/24.swap-nodes-in-pairs.go
@@ -37,3 +37,31 @@ func swapPairs(head *ListNode) *ListNode {
 	}
 	return head
 }
+
+func swapPairs2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	s := head.Next
+	var behind *ListNode
+	for head.Next != nil {
+		headNext := head.Next
+		if behind != nil && behind.Next != nil {
+			behind.Next = headNext
+		}
+		var next *ListNode
+		if head.Next.Next != nil {
+			next = head.Next.Next
+		}
+		if head.Next.Next != nil {
+			head.Next = next
+		} else {
+			head.Next = nil
+		}
+		headNext.Next = head
+		if head.Next != nil {
+			head = next
+		}
+	}
+	return s
+}
