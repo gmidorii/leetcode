859fa232509e2c318c363a1f40c44670f56ff622
gmidorii <stitow14@gmail.com>
2021-05-12 22:56:59 +0900

    update no26.

--- algorithms/26.remove-duplicates-from-sorted-array.go
+++ algorithms/26.remove-duplicates-from-sorted-array.go
@@ -1,33 +1,21 @@
 package main
 
-import "fmt"
-
 func removeDuplicates(nums []int) int {
-	l := len(nums)
+	if len(nums) == 0 || len(nums) == 1 {
+		return len(nums)
+	}
 	behindIdx := 0
-	idx := 1
-	for i := 0; i < l; i++ {
+	for i := 0; i < len(nums); i++ {
 		if i == 0 {
 			continue
 		}
-		if idx+1 > len(nums) {
-			break
-		}
-		fmt.Println(nums)
-		fmt.Println(behindIdx + 1)
-		fmt.Println(len(nums))
-		fmt.Println(idx)
-		fmt.Println(nums[idx])
-		if nums[idx] == nums[behindIdx] {
-			idx++
+		if nums[i] == nums[behindIdx] {
 			continue
 		}
-		fmt.Println("not equal")
-		nums = append(nums[:behindIdx+1], nums[idx:]...)
+		nums[behindIdx+1] = nums[i]
 		behindIdx = behindIdx + 1
 	}
 	nums = nums[:behindIdx+1]
-	fmt.Println(nums)
 	return len(nums)
 }
 
--- algorithms/26.remove-duplicates-from-sorted-array_test.go
+++ algorithms/26.remove-duplicates-from-sorted-array_test.go
@@ -21,24 +21,27 @@ func Test_removeDuplicates(t *testing.T) {
 				nums: []int{1, 1, 2},
 			},
 			want:     2,
-			wantNums: []int{1, 2},
+			wantNums: []int{1, 2, 2},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+			},
+			want:     5,
+			wantNums: []int{0, 1, 2, 3, 4, 2, 2, 3, 3, 4},
 		},
 		{
 			name: "",
 			args: args{
 				nums: []int{1, 1},
 			},
-			want:     2,
-			wantNums: []int{1},
+			want:     1,
+			wantNums: []int{1, 1},
 		},
 	}
 	for _, tt := range tests {
-		before := make([]int, len(tt.args.nums))
-		n := copy(before, tt.args.nums)
-		t.Log(before)
-		t.Log(n)
-		t.Log(tt.args.nums)
-		if got := removeDuplicates2(tt.args.nums); got != tt.want {
+		if got := removeDuplicates(tt.args.nums); got != tt.want {
 			t.Errorf("%q. removeDuplicates() = %v, want %v", tt.name, got, tt.want)
 		}
 		if !reflect.DeepEqual(tt.args.nums, tt.wantNums) {
