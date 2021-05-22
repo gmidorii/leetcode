50f854958838e03079d3ed7d755e391508bc8ca7
gmidorii <stitow14@gmail.com>
2021-05-22 21:31:12 +0900

    add no31 but not clear.

--- algorithms/31.next-permutation.go
+++ algorithms/31.next-permutation.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 /*
 1. 末尾から順番に比較して若いほうが小さい値の場合は入れ替えて返却
 2. 先頭との比較で先頭のほうが小さい場合は、先頭の値より最も近く大きい値と入れ替えて、小さい順にソート
@@ -9,35 +7,65 @@ import "fmt"
 */
 
 func nextPermutation(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+	first := nums[0]
+	max := 101
+	maxIdx := len(nums) - 1
+	maxNext := 101
+	maxNextIdx := len(nums) - 1
+	lastIdx := len(nums) - 1
+	for i := lastIdx; i > 0; i-- {
+		if nums[i] >= first {
+			if nums[i] < max {
+				maxNext = max
+				maxNextIdx = maxIdx
+				max = nums[i]
+				maxIdx = i
+			} else if nums[i] < maxNext {
+				maxNext = nums[i]
+				maxNextIdx = i
+			}
+		}
+		if nums[i] > nums[i-1] {
+			if i == 1 && nums[i] == max {
+				nums[i-1], nums[maxNextIdx] = swap(nums[i-1], nums[maxNextIdx])
+				sortSkipIdx(nums, i)
+				return
+			}
+			nums[i-1], nums[maxIdx] = swap(nums[i-1], nums[maxIdx])
+			sortSkipIdx(nums, i)
+			return
+		}
+	}
+	reverse(nums)
+}
+
+func swap(a, b int) (int, int) {
+	return b, a
+}
+
+func sortSkipIdx(nums []int, firstIdx int) {
+	if len(nums) <= 1 {
+		return
+	}
 	lastIdx := len(nums) - 1
 	for {
-		if lastIdx <= 0 {
+		if lastIdx < firstIdx {
 			break
 		}
-		for i := 0; i < lastIdx; i++ {
-			if lexiCompare(nums[i], nums[i+1]) == -1 {
+		for i := firstIdx; i < lastIdx; i++ {
+			if nums[i] > nums[i+1] {
 				nums[i], nums[i+1] = swap(nums[i], nums[i+1])
-				fmt.Println(swap(nums[i], nums[i+1]))
 			}
 		}
 		lastIdx--
 	}
 }
 
-func lexiCompare(a, b int) int {
-	// 1, 0, -1
-	sa := l[a]
-	sb := l[b]
-	switch {
-	case sa > sb:
-		return -1
-	case sa == sb:
-		return 0
-	default:
-		return 1
+func reverse(nums []int) {
+	for i := 0; i < len(nums)/2; i++ {
+		nums[i], nums[len(nums)-i-1] = swap(nums[i], nums[len(nums)-i-1])
 	}
-}
-
-func swap(a, b int) (int, int) {
-	return b, a
 }
--- algorithms/31.next-permutation_test.go
+++ algorithms/31.next-permutation_test.go
@@ -28,6 +28,55 @@ func Test_nextPermutation(t *testing.T) {
 			},
 			want: []int{1, 2, 3},
 		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 1, 5},
+			},
+			want: []int{1, 5, 1},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1},
+			},
+			want: []int{1},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{2, 6, 5, 4, 3},
+			},
+			want: []int{3, 2, 4, 5, 6},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 2},
+			},
+			want: []int{2, 1},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{5, 4, 7, 5, 3, 2},
+			},
+			want: []int{5, 5, 2, 3, 4, 7},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{2, 3, 1},
+			},
+			want: []int{3, 1, 2},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 5, 1},
+			},
+			want: []int{5, 1, 1},
+		},
 	}
 	for _, tt := range tests {
 		nextPermutation(tt.args.nums)
