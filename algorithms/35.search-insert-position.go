811091acde8006adbbf3d0a4c2738a332c9b010b
gmidorii <stitow14@gmail.com>
2021-05-29 23:28:12 +0900

    add other no35.

--- algorithms/35.search-insert-position.go
+++ algorithms/35.search-insert-position.go
@@ -1,9 +1,44 @@
 package main
 
+import "fmt"
+
 func searchInsert(nums []int, target int) int {
 	for i, n := range nums {
 		if n >= target {
 			return i
+		}
+	}
+	return len(nums)
+}
+
+func searchInsert2(nums []int, target int) int {
+	if len(nums) == 1 {
+		if nums[0] >= target {
+			return 0
+		} else {
+			return 1
+		}
+	}
+	left := 0
+	right := len(nums) - 1
+	for {
+		mid := left + (right-left)/2
+		midP := mid + 1
+		fmt.Printf("l:%v, m:%v, mp:%v, r:%v\n", left, mid, midP, right)
+		fmt.Printf("v l:%v, m:%v, mp:%v, r:%v\n", nums[left], nums[mid], nums[midP], nums[right])
+		if target > nums[mid] && target <= nums[midP] {
+			return midP
+		}
+		if midP == len(nums)-1 {
+			return len(nums)
+		}
+		if mid == 0 {
+			return 0
+		}
+		if nums[mid] < target {
+			left = mid
+		} else {
+			right = mid
 		}
 	}
 	return len(nums)
--- algorithms/35.search-insert-position_test.go
+++ algorithms/35.search-insert-position_test.go
@@ -12,37 +12,51 @@ func Test_searchInsert(t *testing.T) {
 		args args
 		want int
 	}{
+		// {
+		// 	args: args{
+		// 		nums:   []int{1, 3, 5, 6},
+		// 		target: 5,
+		// 	},
+		// 	want: 2,
+		// },
+		// {
+		// 	args: args{
+		// 		nums:   []int{1, 3, 5, 6},
+		// 		target: 2,
+		// 	},
+		// 	want: 1,
+		// },
+		// {
+		// 	args: args{
+		// 		nums:   []int{1, 3, 5, 6},
+		// 		target: 7,
+		// 	},
+		// 	want: 4,
+		// },
+		// {
+		// 	args: args{
+		// 		nums:   []int{1, 3, 5, 6},
+		// 		target: 0,
+		// 	},
+		// 	want: 0,
+		// },
 		{
 			args: args{
-				nums:   []int{1, 3, 5, 6},
-				target: 5,
-			},
-			want: 2,
-		},
-		{
-			args: args{
-				nums:   []int{1, 3, 5, 6},
+				nums:   []int{1},
 				target: 2,
 			},
 			want: 1,
 		},
 		{
 			args: args{
-				nums:   []int{1, 3, 5, 6},
-				target: 7,
-			},
-			want: 4,
-		},
-		{
-			args: args{
-				nums:   []int{1, 3, 5, 6},
-				target: 0,
+				nums:   []int{1},
+				target: 1,
 			},
 			want: 0,
 		},
 	}
 	for _, tt := range tests {
-		if got := searchInsert(tt.args.nums, tt.args.target); got != tt.want {
+		if got := searchInsert2(tt.args.nums, tt.args.target); got != tt.want {
 			t.Errorf("%q. searchInsert() = %v, want %v", tt.name, got, tt.want)
 		}
 	}
