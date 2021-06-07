87dd6abd6e3b5b7dcac15eb9efece009ded5b507
gmidorii <stitow14@gmail.com>
2021-06-07 23:34:51 +0900

    update no39.

--- algorithms/39.combination-sum.go
+++ algorithms/39.combination-sum.go
@@ -1,6 +1,8 @@
 package main
 
-import "sort"
+import (
+	"sort"
+)
 
 func combinationSum(candidates []int, target int) [][]int {
 	var combinations [][]int
@@ -53,19 +55,21 @@ func combinationSum2(candidates []int, target int) [][]int {
 }
 
 func findcombinationSum(nums []int, target, index int, c []int, res *[][]int) {
-	if target <= 0 {
-		if target == 0 {
-			b := make([]int, len(c))
-			copy(b, c)
-			*res = append(*res, b)
-		}
+	// 和が target と一致したとき
+	// target を nums[i] で引き続けているため、0 になったときが一致となる
+	if target == 0 {
+		b := make([]int, len(c))
+		copy(b, c)
+		*res = append(*res, b)
 	}
 	for i := index; i < len(nums); i++ {
+		// target より大きい場合は除く
 		if nums[i] > target {
 			break
 		}
 		c = append(c, nums[i])
 		findcombinationSum(nums, target-nums[i], i, c, res)
+		// c へ追加した数値を除く
 		c = c[:len(c)-1]
 	}
 }
